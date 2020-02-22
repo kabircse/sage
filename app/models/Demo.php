f8273af774d3793f111c349d04c8152e16a36608
Kabir Hossain <kabir.cse10@gmail.com>
2020-02-22 13:09:28 +0600

    last insert id added to democontroller

--- app/models/Demo.php
+++ app/models/Demo.php
@@ -3,17 +3,22 @@
 namespace App\Models;
 
 class Demo extends Model {
-     protected $_table = 'demo_book';
-     public function __construct()
-     {
+    protected $_table = 'demo_book';
+    public function __construct()
+    {
         parent::__construct();
-     }
-     public function get_by_pdo_query() {  //good without param
-         return $this->pdo->query("SELECT * FROM address_book");
-     }
-     public function get_by_pdo_prepare($param) { //good with param
-         $rs = $this->pdo->prepare("SELECT * FROM address_book where id= ?");
-         $rs->execute($param);
-         return $rs->fetchAll();
-     }
+
+        // when id is not primary key then define another column as primary key
+        $this->primary[$this->_table] = 'pk_id';
+    }
+
+    public function get_by_pdo_query() {  //good without param
+        return $this->pdo->query("SELECT * FROM address_book");
+    }
+
+    public function get_by_pdo_prepare($param) { //good with param
+        $rs = $this->pdo->prepare("SELECT * FROM address_book where id= ?");
+        $rs->execute($param);
+        return $rs->fetchAll();
+    }
 }
